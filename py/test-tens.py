cb3098917e27435748b4d3776c54a6ac993d3733
liryi <841194253@qq.com>
2024-10-28 16:31:07 +0800

    test-tens update

--- py/test-tens.py
+++ py/test-tens.py
@@ -9,3 +9,18 @@ b = tf.constant(2.)
 print(a+b)
 print('GPU:', tf.test.is_gpu_available())
 print(tf.config.list_physical_devices('GPU'))
+
+if tf.test.is_gpu_available():
+    print("GPU is available")
+else:
+    print("GPU is not available, using CPU")
+
+print(tf.test.gpu_device_name())
+print(tf.config.experimental.set_visible_devices)
+print('GPU:', tf.config.list_physical_devices('GPU'))
+print('CPU:', tf.config.list_physical_devices(device_type='CPU'))
+print(tf.config.list_physical_devices('GPU'))
+print(tf.test.is_gpu_available())
+# 输出可用的GPU数量
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+# 查询GPU设备
