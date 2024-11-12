7a9a14d253121ca5b3d6ca158ba84ed58eb32a53
liryi <841194253@qq.com>
2024-11-12 23:34:39 +0800

    work6完成

---
+++ exam_LRX/cpp/work6.cpp
@@ -0,0 +1,25 @@
+//
+// Created by liryi on 24-11-12.
+//
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+    // 读取灰度图像
+    cv::Mat image = cv::imread("E:\\DL_class\\exam_LRX\\cpp\\images\\image8.png", cv::IMREAD_GRAYSCALE);
+    if (image.empty()) {
+        std::cerr << "Could not open or find the image!" << std::endl;
+        return -1;
+    }
+
+    // 对灰度图像进行直方图均衡化
+    cv::Mat equalizedImage;
+    cv::equalizeHist(image, equalizedImage);
+
+    // 显示均衡化后的图像
+    cv::imshow("Image", equalizedImage);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+
+    return 0;
+}
