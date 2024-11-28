36da6da49d97452b21bcaf6fc0aa1d7ab357c1c8
liryi <841194253@qq.com>
2024-11-28 16:01:33 +0800

    work6修改

--- exam_latest/exam_CMM/work6.py
+++ exam_latest/exam_CMM/work6.py
@@ -1,18 +1,26 @@
 # 6.采用直方图均衡方法处理图像A，结果类似图B
+from PIL import Image
 import cv2
-import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import pyplot as plt
 
-# 读取图像（假设是灰度图像，如果是彩色图像，需要先转换为灰度）
-image = cv2.imread('image/image6.png', cv2.IMREAD_GRAYSCALE)
+input_path = "image/image6.png"
+original_image = cv2.imread(input_path, cv2.IMREAD_GRAYSCALE)
 
-# 进行直方图均衡化
-equalized_image = cv2.equalizeHist(image)
+equalized_image = cv2.equalizeHist(original_image)
 
-# 显示原图和均衡化后的图像
-plt.figure()
-plt.subplot()
-plt.title('Image')
-plt.imshow(image, cmap='gray')
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.title("Original Image")
+plt.imshow(original_image, cmap="gray")
+plt.axis("off")
+
+plt.subplot(1, 2, 2)
+plt.title("Equalized Image")
+plt.imshow(equalized_image, cmap="gray")
+plt.axis("off")
+
+plt.tight_layout()
 plt.show()
 
 # 逻辑分析
